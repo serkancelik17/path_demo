fb775c83285752102334c6cd771a6442322e5e95
Serkan Celik <serkan.celik@ayakkabionline.com>
2022-06-22 13:59:40 +0300

    added new test functions.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -18,18 +18,4 @@ class IndexController extends AbstractController
             'message' => 'Welcome to Path Demo Project!',
         ]);
     }
-
-//    /**
-//     * @Route("/hash",name="app_hash")
-//     */
-//    public function hash(UserPasswordHasherInterface $passwordHasher) {
-//        $password = 'j6vmk8y43i7z75bk';
-//        $user_id = 1;
-//        $user = $this->getDoctrine()->getRepository(User::class)->find($user_id);
-//
-//        $hashedPassword = $passwordHasher->hashPassword($user,$password);
-//
-//        dump($hashedPassword);
-//        exit;
-//    }
 }
--- tests/Controller/OrderControllerTest.php
+++ tests/Controller/OrderControllerTest.php
@@ -4,12 +4,70 @@
 namespace App\Tests\Controller;
 
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class OrderControllerTest extends WebTestCase
 {
-    public function testIndex()
+    private $client = null;
+    private $userRepository = null;
+
+    public function setUp() : void
     {
+        $this->client = static::createClient();
+        $this->userRepository = static::getContainer()->get(UserRepository::class);
     }
+
+    public function testIndexSuccess()
+    {
+        // if response.status == 2
+        $this->assertTrue(true);
+    }
+
+    public function testShowIsSuccess()
+    {
+        // if response.status == 200
+        $this->assertTrue(true);
+    }
+
+    public function testShowOrderIsNotBelongUser()
+    {
+        // if order is not belongs current user
+        $response = ['success' => false];
+
+        $this->assertFalse($response['success']);
+    }
+
+
+    public function testStoreIsSuccess()
+    {
+
+        //if response.status == 200
+        $response = ["success" => true];
+
+        $this->assertTrue($response['success']);
+
+    }
+
+    public function testStoreIsHasValidationError()
+    {
+
+        //if response.validation == false
+        $response = ["success" => false];
+
+        $this->assertFalse($response['success']);
+
+    }
+
+
+    public function testUpdateIsSuccess()
+    {
+        //if response.status == false
+        $response = ["success" => true];
+
+        $this->assertTrue($response['success']);
+
+    }
+
 
 }
